b46cff4904e7eace411bba0dc32a5e12170dcd85
Blackstock <charlieblackstock@gmail.com>
2015-09-19 08:57:22 -0600

    refactored jQuery forms

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 $(document).on('ready', function() {
+
   $("#addGame").on("click", function(e) {
     e.preventDefault();
     $title = $("#title").val();
     $genre = $("#genre").val();
     var newGame = new Game($title, $genre);
     newGame.render();
+  });
+
+  $("#removeGame").on("click", function(e) {
+    e.preventDefault();
+    $lastGame = $("form").find("div").length - 1;
+    $("form").find("div").eq($lastGame).remove();
   });
 });
 
@@ -19,8 +26,8 @@ var Game = function(title, genre) {
 };
 
 Game.prototype.render = function () {
-  $('form').append("<li>Title: " + this.title + "</li>");
-  $('form').append("<li>Genre: " + this.genre + "</li>");
+  $('form').append("<div><li>Title: " + this.title + "</li><li></div>");
+  $('form').append("<li>Genre: " + this.genre + "</li><br>");
 };
 
 GameLibrary.prototype.render = function () {
